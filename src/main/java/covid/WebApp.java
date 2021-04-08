74636d1ae13c0f1bde485c7f749ab2ba19b70518
Stéphane <81stephane@gmail.com>
2021-04-08 15:52:40 +0200

    Téléchargement et lecture fichier OWD

--- src/main/java/covid/WebApp.java
+++ src/main/java/covid/WebApp.java
@@ -1,10 +1,15 @@
 package covid;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.MalformedURLException;
+import au.com.bytecode.opencsv.CSVReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.Level;
@@ -15,25 +20,54 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebApp.class, args);
-                
-                // Timer
-                Timer timer = new Timer();
-                TimerTask dailyTask = new TimerTask() {
-                    // On télécharge le fichier toutes les 24h
-                    @Override
-                    public void run() {
-                        // Code
-                        try {
-                            InputStream input = new URL("https://covid.ourworldindata.org/data/owid-covid-data.csv").openStream();
-                            Reader reader = new InputStreamReader(input, "UTF-8");
-                        } catch (Exception ex) {
-                            Logger.getLogger(WebApp.class.getName()).log(Level.SEVERE, null, ex);
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp.class, args);
+
+        // Timer
+        Timer timer = new Timer();
+        TimerTask dailyTask = new TimerTask() {
+            // On télécharge le fichier toutes les 24h
+            @Override
+            public void run() {
+                // Code
+                try {
+                    // Téléchargement et stockage du fichier OWD
+                    ReadableByteChannel readChannel = Channels.newChannel(new URL("https://covid.ourworldindata.org/data/owid-covid-data.csv").openStream());
+                    FileOutputStream fileOS = new FileOutputStream("dataOWD.csv");
+                    FileChannel writeChannel = fileOS.getChannel();
+                    writeChannel
+                            .transferFrom(readChannel, 0, Long.MAX_VALUE);
+
+                    // Traitement du fichier OWD
+                    File fileOWD = new File("dataOWD.csv");
+                    FileReader frOWD = new FileReader(fileOWD);
+                    CSVReader csvrOWD = new CSVReader(frOWD, ';');
+
+                    // Lecture du fichier OWD
+                    List<String[]> dataOWD = new ArrayList<String[]>();
+                    String[] nextLine = null;
+                    while ((nextLine = csvrOWD.readNext()) != null) {
+                        int size = nextLine.length;
+                        if (size == 0) {
+                            continue;
                         }
+                        String debut = nextLine[0].trim();
+                        if (debut.length() == 0 && size == 1) {
+                            continue;
+                        }
+                        if (debut.startsWith("#")) {
+                            continue;
+                        }
+                        dataOWD.add(nextLine);
                     }
-                };
-                // Timer de 24h
-                timer.schedule(dailyTask, 01, 24*60*60*1000);
-	}
+
+
+                } catch (Exception ex) {
+                    Logger.getLogger(WebApp.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        };
+        // Timer de 24h
+        timer.schedule(dailyTask, 01, 60 * 60);
+    }
 }
