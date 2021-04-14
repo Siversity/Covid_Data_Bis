2e89ff19442b60de5bc9a160a8ca9da83e94430b
lisa <lisa.estebe@gmail.com>
2021-04-14 15:45:18 +0200

    CountryController

--- src/main/java/covid/controller/CountryController.java
+++ src/main/java/covid/controller/CountryController.java
@@ -1,10 +1,24 @@
 package covid.controller;
 
+import covid.dao.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = "/country")
 public class CountryController {
     
+    @Autowired
+    private CountryRepository countryDao;
+    
+    @GetMapping(path="show")
+    public String afficheTousCountries(Model model){
+        model.addAttribute("countries", countryDao.findAll());
+        return "europe";
+    }
 }
+
+
