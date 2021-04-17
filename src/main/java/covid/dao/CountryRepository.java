c7c744d983008e0cb2b671fe6ca86ac1dc543772
Stéphane <81stephane@gmail.com>
2021-04-17 14:21:12 +0200

    Requête DAO

--- src/main/java/covid/dao/CountryRepository.java
+++ src/main/java/covid/dao/CountryRepository.java
@@ -1,20 +1,18 @@
 package covid.dao;
 
+import covid.entity.Continent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import covid.entity.Country;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 // This will be AUTO IMPLEMENTED by Spring 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, String> {
 
-    /*
-    @Query("SELECT * AS countries "
-            + "FROM Country c "
-            + "WHERE c.continent.nameContinent = :nameContinent ")
-    public List<Country> findByContinent(String nameContinent);
-    */
+    @Query(value = "SELECT * FROM Country WHERE Continent = :nameContinent", nativeQuery = true)
+        List<Country> getEuropeanCountries(String nameContinent);
 
 }
